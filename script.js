1bc604767fad0d99cb4ae570a552e9fc5b7f7850
RM-0311 <66578387+RM-0311@users.noreply.github.com>
2023-09-24 18:39:09 -0500

    Add Calculator

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ $(document).ready(function () {
     );
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const initialInvestmentInput = document.getElementById("initial-investment");
+  const revenueInput = document.getElementById("revenue");
+  const expensesInput = document.getElementById("expenses");
+  const calculateButton = document.getElementById("calculate-button");
+  const roiResult = document.getElementById("roi-result");
+
+  calculateButton.addEventListener("click", function () {
+      // Get input values
+      const initialInvestment = parseFloat(initialInvestmentInput.value);
+      const revenue = parseFloat(revenueInput.value);
+      const expenses = parseFloat(expensesInput.value);
+
+      // Check for valid inputs
+      if (isNaN(initialInvestment) || isNaN(revenue) || isNaN(expenses)) {
+          roiResult.textContent = "Please enter valid numbers for all fields.";
+          return;
+      }
+
+      // Calculate ROI
+      const roi = ((revenue - expenses - initialInvestment) / initialInvestment) * 100;
+
+      // Display the result rounded to two decimal places
+      roiResult.textContent = `ROI: ${roi.toFixed(2)}%`;
+  });
+});
