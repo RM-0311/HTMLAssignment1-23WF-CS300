9a340abbebe05464f827f547926f0e5468dd4835
RM-0311 <66578387+RM-0311@users.noreply.github.com>
2023-09-24 18:31:43 -0500

    JS added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+$(document).ready(function () {
+  // Initially hide the investment details
+  $('#investment-details').hide();
+
+  // Toggle visibility of the investment details
+  $('#toggle-investment').click(function () {
+    $('#investment-details').slideToggle();
+    var buttonText = $('#toggle-investment').text();
+    $('#toggle-investment').text(
+      buttonText === 'Read More' ? 'Read Less' : 'Read More'
+    );
+  });
+});
